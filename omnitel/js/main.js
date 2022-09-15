15da5e37f6e3aa6fcaeeddf84897fd420f5040f0
okfaji <okfaji92@gmail.com>
2022-09-16 01:07:08 +0900

    .

--- omnitel/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.works ul li').on('mouseenter', function(){
-        $('.works ul li').removeClass('off');
-        $('.works ul li').removeClass('on');
-        $(this).addClass('on');
-        $('.works ul li:not(.on)').addClass('off');
-    });
-});
