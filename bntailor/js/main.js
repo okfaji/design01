b9f59cb7325d22d787aeec1ba28d87cce50b5f65
okfaji <okfaji92@gmail.com>
2022-10-27 12:17:57 +0900

    .

--- bntailor/js/main.js
+++ bntailor/js/main.js
@@ -15,5 +15,25 @@ $(document).ready(function(){
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
         },
 
+    });// visual swiper
+
+    /* visual popup의 정지버튼/재생버튼 
+        하나의 버튼이 두가지 기능
+        정지와 재생 기능을 구분하는 값.... 
+        btn_stop 버튼에 play클래스가 없으면 일시정지
+        play클래스가 있으면 재생
+    */
+    $('.visual .popup .btn_stop').on('click', function(){
+        let popStatus = $(this).hasClass('play'); //true면 play가 있다는 이야기 - 재생기능실행
+        if(popStatus == true){ //버튼의 상태가 play모양 - 현재 정지상태 - 재생기능을 실행
+            swiper.autoplay.start();
+            $(this).removeClass('play');
+            $(this).text('일시정지');
+        }else{//버튼의 상태가 일시정지 모양 - 현재 재생상태 - 일시정지 기능을 실행
+            swiper.autoplay.stop();
+            $(this).addClass('play');
+            $(this).text('재생');
+        }
     });
+
 });
