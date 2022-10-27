74627bdc4cab21d810b3a371f5f7258a1c8c03d8
okfaji <okfaji92@gmail.com>
2022-10-27 10:00:35 +0900

    .

---
+++ behr_ani/js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    let winW = $(window).width()
+    let docW = $(document).width()
+
+    // $('header').addClass('fixed')
+    // $('header').removeClass('fixed')
+
+    //TOP버튼을 클릭했을때 상단으로 스크롤
+    $('aside button').on('click', function(){
+        console.log('눌렀어')
+        // $(window).scrollTop(100)
+        $('html,body').animate({
+            scrollTop : 0
+        },500)
+    })
+
+    /*
+        header에
+        조건 1 - 스크롤 값이 0보다 크면 header에 fixed 클래스 추가
+        조건 2 - 스크롤 값이 0이면 header에 fixed 클래스를 삭제
+    */
+    /* 로딩했을때 맨 처음에만 실행을 단 한번 */
+    let scrolling
+    headerFixed()//함수의 실행
+
+    /* 스크롤 할때마다 실행 - 스크롤 안하면 실행X */
+    $(window).scroll(function(){ 
+        headerFixed()//함수의 실행
+    })
+
+    function headerFixed(){ //함수의 선언
+        scrolling = $(window).scrollTop()
+        if(scrolling > 0){
+            $('header').addClass('fixed')
+        }else{
+            $('header').removeClass('fixed')
+        }
+    }
+
+    /*
+        header nav에 마우스를 올리면
+        header에 open 클래스를 추가함
+    */
+    $('header nav > ul').on('mouseenter focusin', function(){
+        $('header').addClass('open')
+    })
+    $('header').on('mouseleave', function(){
+        $('header').removeClass('open')
+    })
+    $('header nav > ul > li:last-child > ul > li:last-child').on('focusout', function(){
+        $('header').removeClass('open')
+    })
+
+    let visTop = $('.visual').position().top;
+    console.log(visTop);
+    let proTop = $('.product .list ul li:nth-child(3)').position().top;
+    console.log(proTop);
+    $('.visual .bg img').animate({
+        'transform': 'translateY(100px)'
+    },300);
+    $(window).scroll(function(){
+
+    });
+})
+
